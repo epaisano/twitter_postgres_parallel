9575a8a45111c2310a0563fe03ac991e30c5e61b
Emma Munoz <epaisano24@cmc.edu>
2024-04-08 23:09:33 -0700

    norm parallel again

--- load_normalized.sh
+++ load_normalized.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-unzip -p "$1" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:62725/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:62725 --inputs="$1"
